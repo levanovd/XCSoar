8fff3c3f37e4f4cdb1663fa053d0b320138ba602
Dmitrii Levanov <levanovd@gmail.com>
2025-03-19 15:04:53 +0100

    Add TrafficHistory.cpp

--- build/main.mk
+++ build/main.mk
@@ -233,6 +233,7 @@ XCSOAR_SOURCES := \
 	$(SRC)/FLARM/FlarmNetDatabase.cpp \
 	$(SRC)/FLARM/FlarmNetReader.cpp \
 	$(SRC)/FLARM/Traffic.cpp \
+	$(SRC)/FLARM/TrafficHistory.cpp \
 	$(SRC)/FLARM/Calculations.cpp \
 	$(SRC)/FLARM/Friends.cpp \
 	$(SRC)/FLARM/Computer.cpp \
